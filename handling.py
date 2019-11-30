7951b2c11bf57daa82e8c2da5e69844a7c3b69ba
kangsungsu <51367515+kangsungsu@users.noreply.github.com>
2019-11-30 21:28:19 +0900

    Update handling.py

--- handling.py
+++ handling.py
@@ -4,8 +4,8 @@ from time import sleep
 lm = LargeMotor('outC'); assert lm.connected
 rm = LargeMotor('outD'); assert rm.connected 
 
-direct=int(input("방향을 입력하세요"))
-
+direct=int(input("방향각을 입력하세요(왼쪽기준"))
+direct=direct*20/9
 lm.stop(stop_action = "brake")
 rm.stop(stop_action = "brake")
 
@@ -14,8 +14,8 @@ rm.run_to_rel_pos(position_sp = -200, speed_sp = 300, stop_action = "brake")
             
 sleep(1)
 
-lm.run_to_rel_pos(position_sp = -190, speed_sp = 300, stop_action = "brake")
-rm.run_to_rel_pos(position_sp = 190, speed_sp = 300, stop_action = "brake")
+lm.run_to_rel_pos(position_sp = direct, speed_sp = 300, stop_action = "brake")
+rm.run_to_rel_pos(position_sp = direct, speed_sp = 300, stop_action = "brake")
 
                         
         
