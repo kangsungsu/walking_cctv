b1e1bb2de81cc13a465a490af5829d986dd2a2fa
kangsungsu <51367515+kangsungsu@users.noreply.github.com>
2019-11-30 21:34:00 +0900

    Update walking_cctv.py

--- walking_cctv.py
+++ walking_cctv.py
@@ -78,7 +78,7 @@ class robot:
 
                 sleep(2)
                
-            elif (angle < -500)and (right_1>2400)and (right_2>2400):
+            elif (angle < -500)or ((right_1>2200)and (right_2>2200)):
                 print("angle")
                 lm.stop(stop_action = "brake")
                 rm.stop(stop_action = "brake")
